f1a4f36d37c4b9cde26c17559e157158b81cb474
Qababa1000 <deltagare@LEXJONKAF07>
2020-09-13 20:46:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace assignmentCalculateAnumber
+{
+    class Program
+    {
+        // declaring a method
+        static void CompereNumbers ()
+
+        {
+            int numberOne;
+            int numberTwo;
+
+            Console.WriteLine("Please enter a number: ");
+            numberOne = TextToNumber(Console.ReadLine());
+
+            Console.WriteLine("Please enter a number: ");
+            numberTwo = TextToNumber(Console.ReadLine()); 
+
+            if (numberOne > numberTwo )
+            {
+                Console.WriteLine("First number is the biggest");
+
+            }
+            else
+            { 
+                Console.WriteLine("Second number is the biggest");
+            
+            }
+
+
+
+        }
+        static int TextToNumber (string textNumber)
+        {
+            int number = 0;
+            int.TryParse(textNumber, out number);
+            return number;
+
+        }
+    }
+}
